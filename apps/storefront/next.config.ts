2f7efbc382efe9c84a16dbf7be7a7d966965c11a
Soheil Movahedan <soheil.movahhedan@gmail.com>
2026-05-17 00:26:35 +0200

    chore(storefront): remove eslint ignoreDuringBuilds option from next.config.ts

--- apps/storefront/next.config.ts
+++ apps/storefront/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
 	typescript: {
 		ignoreBuildErrors: true,
 	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
 
 	// Enable hot reload in Docker containers
 	webpack: (
